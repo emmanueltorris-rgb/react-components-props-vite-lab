ccfbf58725f2cf6a1432032d11885d2a4ad7b60f
Emmanuel <emmanuel.torris@student.moringaschool.com>
2026-05-13 12:54:55 +0300

    code for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import blogData from "./data/blog";
-import Header from "./components/Header";
-import About from "./components/About";
-import ArticleList from "./components/ArticleList";
-import Article from "./components/Article";
-
-console.log(blogData);
+import './App.css';
+import blog from './data/blog';
+import Header from './components/Header';
+import About from './components/About';
+import ArticleList from './components/ArticleList';
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div className="app">
+      <Header name={blog.name} />
+      
+      <About image={blog.image} about={blog.about} />
+      
+      <ArticleList posts={blog.posts} />
     </div>
   );
 }
